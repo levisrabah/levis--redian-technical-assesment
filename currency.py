dbe2bb93df20a3268549fabe504f02c52130b611
levisrabah <levisrabah@gmail.com>
2026-02-24 11:37:17 +0300

    create a python file to test the API and initialize the project

---
+++ currency.py
@@ -0,0 +1,8 @@
+# # Currency conversion test
+# import requests
+# url = 'https://open.er-api.com/v6/latest'
+
+# # Making our request
+# response = requests.get(url)
+# data = response.json()
+# print (data)
